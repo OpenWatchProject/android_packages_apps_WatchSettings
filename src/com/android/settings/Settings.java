37e2f27c4848f748739def85a2bd540c20724b9b
Chris <chris@cypheros.co>
2017-12-09 18:07:02 -0500

    BatteryLightSettings: Clean up interface and presentation
    This commit allows the batterylight preference to match the system
    standard preferences in the settings menu, with a small touch up on
    the color items. This fits in with the remaining custom preferences,
    and limits the usage of PreferenceController.
    
    - Created a basic style to reference
    - Moved the color item to the left of the preference (Icon reserve)
    - Increased the size of the color item
    - Changed the color item shape from rectangle to oval
    - Converted Battery Light settings into a SettingsActivity
    - Added switchbar for enabling the battery light
    - Added a master switch (Design guideline for switchbar fragments)
    - Added dynamic summary for switch state
    - Added preference footer
    
    Change-Id: Iebda5f8148a552e0d8dd7c376efdd6e27dfed00a

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -193,4 +193,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
 
     public static class CarbonFibersActivity extends SettingsActivity { /* empty */ }
+    public static class BatteryLightSettingsActivity extends SettingsActivity { /* empty */ }
 }

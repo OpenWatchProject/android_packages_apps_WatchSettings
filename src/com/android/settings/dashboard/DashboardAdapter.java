f355286a859f6104126b123f7e12dc0b1211696a
Iscle <albertiscle9@gmail.com>
2018-08-09 01:27:09 +0200

    Fix imports
    This fixes the following error:
    LinearLayout cannot be resolved to a type

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -32,7 +32,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout.LayoutParams;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.logging.MetricsLogger;
